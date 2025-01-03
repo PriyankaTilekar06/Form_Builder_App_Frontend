57a577ba70989e2b993a49e55822d4fd97f89622
Priyanka <priyankatilekar003@gmail.com>
2025-01-03 06:05:37 +0530

    committed

--- context/userContext.jsx
+++ context/userContext.jsx
@@ -5,31 +5,31 @@ export const UserContext = createContext({})
 
 export function UserContextProvider({children}) {
     const [user, setUser] = useState(null)
-    useEffect(() => {
-        if(!user){
-            axios.get('/profile', {withCredentials: true}).then(({data}) => {
-                setUser(data)
-            })
-        }
-    }, [])
     // useEffect(() => {
-    //     if (!user) {
-    //         // axios.get('/auth/profile', {withCredentials: true} )
-    //         axios.get('/profile', {
-    //             headers: {
-    //                 Authorization: `Bearer ${localStorage.getItem('token')}`,
-    //             },
-    //         }, {withCredentials: true})
-    //             .then(({ data }) => {
-    //                 console.log('Fetched User:', data);
-    //                 setUser(data); 
-    //             })
-    //             .catch((error) => {
-    //                 console.error('Profile fetch error:', error.message);
-    //                 setUser(null); // Fallback to null if the request fails
-    //             });
+    //     if(!user){
+    //         axios.get('/profile', {withCredentials: true}).then(({data}) => {
+    //             setUser(data)
+    //         })
     //     }
-    // }, []);
+    // }, [])
+    useEffect(() => {
+        if (!user) {
+            // axios.get('/auth/profile', {withCredentials: true} )
+            axios.get('/profile', {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`,
+                },
+            }, {withCredentials: true})
+                .then(({ data }) => {
+                    console.log('Fetched User:', data);
+                    setUser(data); 
+                })
+                .catch((error) => {
+                    console.error('Profile fetch error:', error.message);
+                    setUser(null); // Fallback to null if the request fails
+                });
+        }
+    }, []);
     return(
         <UserContext.Provider value={{user, setUser}}>
             {children}
